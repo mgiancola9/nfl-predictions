52fbba19b7d686c6d7d87df9505757aeddb80073
HarryEaston <harry.easton@icloud.com>
2025-12-14 02:13:32 -0500

    new branch

--- src_spread/data/__init__.py
+++
@@ -1,5 +0,0 @@
-# src/data/__init__.py
-
-from .preprocess_data import main as process_data_pipeline
-
-__all__ = ["process_data_pipeline"]
--- src_spread/model/__init__.py
+++
@@ -1,6 +0,0 @@
-# src/model/__init__.py
-
-# Placeholder for now until you update train_model.py
-# from .train_model import train_walk_forward
-
-__all__ = []
